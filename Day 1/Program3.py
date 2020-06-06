92aa02621c104c5644b254465d8c759897963e03
AkshaySingh566 <59649548+AkshaySingh566@users.noreply.github.com>
2020-06-06 10:36:27 +0530

    Add files via upload

---
+++ Day 1/Program3.py
@@ -0,0 +1,7 @@
+import math
+a = int(input("Enter the 1st value: "))
+b = int(input("Enter the 2nd value: "))
+a=a+b
+b=a-b
+a=a-b
+print("Swaped nos:\n1st value = ",a,"\n2nd value = ",b)
---
+++ Day 1/Program4.py
@@ -0,0 +1,5 @@
+import math
+a = float(input("Enter the Cost Price:"))
+b = float(input("Enter the Selling Price:"))
+print("Profit is:",(b-a)*100/a,"%")
+print("The selling price, if profit is 5%, is:",a*21/20)
